22a35aa9becada324a6669b9c8b9a09a59de097b
gilcota <gilemmmc@hotmail.com>
2024-05-26 20:51:07 -0600

    Preload none audio

--- src/js/PokemonList.mjs
+++ src/js/PokemonList.mjs
@@ -2,8 +2,8 @@ import { renderListWithTemplate } from "./utils.mjs";
 
 function pokemonCardTemplate(pokemon) {
     return `<div class="pokemon-card">
-        <img src ="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" loading="lazy" alt="Image of ${pokemon.name}" class="pimage"/>
-        <audio class="roar" width="100%" height="auto" controls controlsList="nodownload noplaybackrate">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" loading="lazy" alt="Image of ${pokemon.name}" class="pimage"/>
+        <audio class="roar" preload="none" width="100%" height="auto" controls controlsList="nodownload noplaybackrate">
             <source src="https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${pokemon.id}.ogg" type="audio/ogg">
         </audio>
         <h1 id="pokemon-id">#${pokemon.id}</h1>
